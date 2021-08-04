9ceae99f14ff084e78ed31ac039769d1f9b18561
DarkWater <daliskandarov.ru@mail.ru>
2021-08-04 14:58:48 +0300

    fix

---
+++ script.js
@@ -0,0 +1,14 @@
+let col_obj = document.querySelector("#col");
+let text_obj = document.querySelector("#text");
+let rezult_obj = document.querySelector("#text_izv");
+let submit_obj = document.querySelector("#submit");
+
+console.log(submit_obj)
+
+submit_obj.addEventListener("click", function(){
+    rezult_obj.value = "";
+    if(col_obj.value && text_obj.value)
+        for(let i = 0; i < col_obj.value; i++){
+            rezult_obj.value += text_obj.value + "\n";
+        }
+})
